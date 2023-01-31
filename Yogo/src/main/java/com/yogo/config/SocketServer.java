401b6e899caa0f7b74e93f948757c1ec4c8fd19b
VinhNguyen217 <nguyenducvinhthd@gmail.com>
2023-01-31 14:06:25 +0700

    fix socket connect

--- Yogo/src/main/java/com/yogo/config/SocketServer.java
+++ Yogo/src/main/java/com/yogo/config/SocketServer.java
@@ -7,7 +7,11 @@ import com.corundumstudio.socketio.SocketIOServer;
 import com.corundumstudio.socketio.listener.ConnectListener;
 import com.corundumstudio.socketio.listener.DisconnectListener;
 import com.yogo.business.socket.SocketClientManage;
+import com.yogo.business.socket.SocketDriverManage;
+import com.yogo.business.socket.UserSocket;
 import lombok.extern.log4j.Log4j2;
+
+import java.util.Optional;
 
 @Log4j2
 public class SocketServer {
@@ -34,7 +38,24 @@ public class SocketServer {
         socket.addDisconnectListener(new DisconnectListener() {
             @Override
             public void onDisconnect(SocketIOClient client) {
-                log.info("disconnect with " + client);
+                Optional<UserSocket> userSocketClient = SocketClientManage
+                        .getInstance()
+                        .list
+                        .stream().filter(u -> client == u.getSocketIOClient())
+                        .findFirst();
+                if (userSocketClient.isPresent()) {
+                    SocketClientManage.getInstance().list.remove(userSocketClient.get());
+                } else {
+                    Optional<UserSocket> userSocketDriver = SocketDriverManage
+                            .getInstance()
+                            .list
+                            .stream().filter(u -> client == u.getSocketIOClient())
+                            .findFirst();
+                    userSocketDriver
+                            .ifPresent(userSocket -> SocketDriverManage.getInstance().list.remove(userSocket));
+                }
+                log.info("list of clients : " + SocketClientManage.getInstance().list.size());
+                log.info("list of drivers : " + SocketDriverManage.getInstance().list.size());
             }
         });
         return socket;
